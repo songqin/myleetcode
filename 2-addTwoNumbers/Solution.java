af4b77df68fb36f7be0af32379f0548cff98cf0c
Song <richieqin@gmail.com>
2015-11-28 19:48:35 -0500

    init

--- 2-addTwoNumbers/Solution.java
+++ 2-addTwoNumbers/Solution.java
@@ -1,32 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
 /*
-Easy
+Input: (6 -> 4 -> 9) + (7 -> 6 -> 4)
+Output: 3 ->1-> 4 c:1  
 
-Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
 
-For example:
+Input: (2 -> 4 ) + (5 -> 6 -> 4)
+Output: 7-> 1,1->
 
-Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
 
-Follow up:
-Could you do it without any loop/recursion in O(1) runtime?
+Input: (2 -> 4 -> 3) + (5 -> 6 )
+Output: 7 -> 0 -> 8   
 */
 public class Solution {
-	public int sumDigits(int num){
-    	int sum=0;
-    	while(num>0){
-    		sum=sum+num%10;
-    		num/=10;
-    	}
-    	return sum;
-	}
-    public int addDigits(int num) {
-    	while(num>=10)
-    		num=sumDigits(num);
-    	return num;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carryOver=0;
+        ListNode l3=null;
+        while(l1!=null && l2!=null)
+        {
+            l3=new ListNode((l1.val+l2.val+carryOver)%10);
+            carryOver=(l1.val+l2.val)/10;
+            
+            l1=l1.next;
+            l2=l2.next;
+            l3=l3.next;
+        }
+        if(l1==null && l2!=null){
+            l3=new ListNode(l2.val+carryOver);
+        }
+        else if(l2==null && l1!=null){
+            l3=new ListNode(l1.val+carryOver);
+        }
+        else if (carryOver>0){
+            l3=new ListNode(carryOver);
+        }
+
+        return l3;
+        
+
     }
     public static void main(String [] args){
-		Solution s = new Solution();
-		int r = s.addDigits(103);
-		System.out.println(r);
+        Solution s = new Solution();
+        // int r = s.addDigits(103);
+        // System.out.println(r);
     }
 }
