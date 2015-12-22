a3ba4dfa5ca034535c3434dc10a7ebec78f185a7
Song <richieqin@gmail.com>
2015-12-22 17:49:47 -0500

    no message

--- 3-lengthOfLongestSubstring/Solution.java
+++ 3-lengthOfLongestSubstring/Solution.java
@@ -1,12 +1,22 @@
+//?
 public class Solution {
 	public int lengthOfLongestSubstring(String s) {
 		int start=0, length=0;
 		for(int i=0;i<s.length();i++){
 			for(int j=i+1;j<s.length()-1;j++){
 				if(s.charAt(i)==s.charAt(j)){//forgot String.charAt. 
-					for(int k=1;k<)
+					for(int k=1;k<s.length()-j-1;k++){
+						if(s.charAt(i+k)==s.charAt(j+k)){
+							len++;
+						}
+						else{
+							break;
+						}
+					}
 				}
-				else{}
+				else{
+
+				}
 			}
 		}
 		return 0;
