628aa5efd7d1cca7a8dbef8ea7b4dabc9e8ecace
Song <richieqin@gmail.com>
2015-12-22 17:46:32 -0500

    no message

--- 1-twoSum/Solution.java
+++ 1-twoSum/Solution.java
@@ -7,7 +7,7 @@ You may assume that each input would have exactly one solution.
 
 Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
-?
+?？
 */
 import java.util.HashMap;
 import java.util.Arrays;
