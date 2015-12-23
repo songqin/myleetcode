bef13dbfb33c855ec30c7293dcee6e370ed55652
Song <richieqin@gmail.com>
2015-12-22 19:37:37 -0500

    v2 accepted

--- 3-lengthOfLongestSubstring/Solution.java
+++ 3-lengthOfLongestSubstring/Solution.java
@@ -1,28 +1,45 @@
-//1st Version
+//v.2.0
 public class Solution {
 	public int lengthOfLongestSubstring(String s) {
-		int start=0, length=0;
+		if(s.length()==1) return 1;
+		if(s==null) return 0;
+		boolean [] check=new boolean[256];
+		int maxLen=0,maxStart=0,len=0,start=0;
+		//scan from left to right with interval of length 1
 		for(int i=0;i<s.length();i++){
-			for(int j=i+1;j<s.length()-1;j++){
-				if(s.charAt(i)==s.charAt(j)){//forgot String.charAt. 
-					for(int k=1;k<s.length()-j-1;k++){
-						if(s.charAt(i+k)==s.charAt(j+k)){
-							len++;
-						}
-						else{
-							break;
-						}
-					}
+			java.util.Arrays.fill(check, false);
+			len=0;
+			start=i;
+			for(int j=i;j<s.length();j++){
+				char c =s.charAt(j);
+				if(check[(int)c]){
+					len=j-i;//did not clarify j-i-1 or j-i
+					start=i;
+					break;
 				}
 				else{
-
+					check[(int)c]=true;
 				}
+				len=j-i+1;
 			}
+			
+			if(len>maxLen){
+				maxLen=len;
+				maxStart=start;
+			}
+
+
 		}
-		return 0;
+		// for(int i=0;i<256;i++){
+		// 	char c=(char)i;
+		// 	int e = (int)c;
+		// 	System.out.println(e);
+		// }
+		return maxLen;
 	}
 	public static void main(String [] args){
 		Solution s = new Solution();
-        // System.out.println(s.lengthOfLongestSubstring())
-    }
+		int l = s.lengthOfLongestSubstring("au");//test "a", "au" 
+		System.out.println(l);
+	}
 }
